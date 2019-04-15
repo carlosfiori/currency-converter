811597fed6779f1bdc8bb6676a58e7d8ee1c6e96
Carlos Fiori <carlos.fiori@gmail.com>
2019-04-15 01:05:40 -0300

    Disabled exception handler on tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,50 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    private $oldExceptionHandler;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->disableExceptionHandling();
+    }
+
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        $this->app->instance(
+            ExceptionHandler::class,
+            new class extends Handler
+            {
+                public function __construct()
+                {
+                }
+
+                public function report(\Exception $e)
+                {
+                }
+
+                public function render($request, \Exception $e)
+                {
+                    throw $e;
+                }
+            }
+        );
+    }
+
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+        return $this;
+    }
 }
